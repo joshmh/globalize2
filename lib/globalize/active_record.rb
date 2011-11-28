4b875037cfaac57863d9a28ce8905756df387f43
Scott Willson <scott.willson@gmail.com>
2011-11-28 13:25:19 -0800

    Replace deprecated class_name call

--- lib/globalize/active_record.rb
+++ lib/globalize/active_record.rb
@@ -54,7 +54,7 @@ module Globalize
 
         after_save :save_translations!
         has_many :translations, :class_name  => translation_class.name,
-                                :foreign_key => class_name.foreign_key,
+                                :foreign_key => self.name.foreign_key,
                                 :dependent   => :delete_all,
                                 :extend      => HasManyExtensions
 
